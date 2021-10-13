199445b1c5c7a7ad86171e445dc3fe07ecb6635f
lonewolf2580 <linusngwee@yahoo.com>
2021-10-13 12:23:39 -0700

    Props - 2 files modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import React, {Component} from 'react'
 import Table from './Table'
 class App extends Component {
   render() {
+    const characters = [
+      {
+        name: 'Don Corleorne',
+        job: 'Godfather'
+      },
+      {
+        name: 'Luca Brazi',
+        job: 'Hitman'
+      },
+      {
+        name: 'Clemenza',
+        job: 'Hitman'
+      },
+      {
+        name: 'Barzini',
+        job: 'Mobster'
+      },
+    ]
     return (
       <div className="container">
-        <Table />
+        <Table characterData={characters} />
       </div>
     )
   }
